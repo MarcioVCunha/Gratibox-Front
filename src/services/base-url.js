9e9723fbdf647c08dab8e67e2d0a4e2e35a79b13
Marcio <marcioabcd11@gmail.com>
2021-11-19 12:17:23 -0300

    home page done

--- src/services/base-url.js
+++ src/services/base-url.js
@@ -3,8 +3,7 @@ import process from 'process';
 const { NODE_ENV } = process.env;
 
 const BASE_URL = (NODE_ENV === 'production')
-  ? 'https://tsuru-store.herokuapp.com'
+  ? 'https://gratibox.herokuapp.com'
   : 'http://localhost:4001';
-
 
 export default BASE_URL;
